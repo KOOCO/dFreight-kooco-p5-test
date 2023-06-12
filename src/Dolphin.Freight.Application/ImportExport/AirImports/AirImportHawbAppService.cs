acc3b082db5e1e0ec79258c6d4ee857450234950
Anand Parmar <anand.parmar@zobiwebsolutions.com>
2023-06-12 15:45:24 +0530

    Build error resolve in master branch.

--- src/Dolphin.Freight.Application/ImportExport/AirImports/AirImportHawbAppService.cs
+++ src/Dolphin.Freight.Application/ImportExport/AirImports/AirImportHawbAppService.cs
@@ -80,6 +80,18 @@ namespace Dolphin.Freight.ImportExport.AirImports
             return retVal;
         }
 
+        public async Task<AirImportHawbDto> GetDocCenterCardById(Guid Id)
+        {
+            if (await _repository.AnyAsync(x => x.Id == Id))
+            {
+                var data = await _repository.GetAsync(f => f.Id == Id);
+                var retVal = ObjectMapper.Map<AirImportHawb, AirImportHawbDto>(data);
+                return retVal;
+            }
+
+            return new AirImportHawbDto();
+        }
+
         public async Task<PagedResultDto<AirImportHawbDto>> QueryListAsync(QueryHblDto query)
         {
             var SysCodes = await _sysCodeRepository.GetListAsync();
